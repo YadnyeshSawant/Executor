d9a6e58eeef0584aa0ccefda60c003ea07a6f9ec
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-09-29 15:10:26 +0530

    Code Formating

--- src/main/java/executor/App.java
+++ src/main/java/executor/App.java
@@ -1,9 +1,8 @@
 package executor;
 
-public class App 
-{
-    public static void main( String[] args )
-    {        
+public class App {
+
+    public static void main(String[] args) {
         PythonExecutor.main(args);
     }
 }
